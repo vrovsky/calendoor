ad146b2850444ab9b764259c77058fa6dcb7ae47
Viachaslau Rouski <vrovsky@gmail.com>
2025-05-27 08:53:09 +0100

    don-bd-schema

---
+++ drizzle/schema.ts
@@ -0,0 +1,69 @@
+import { DAYS_OF_WEEK_IN_ORDER } from "@/constants";
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  index,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+const createdAt = timestamp("createdAt").notNull().defaultNow();
+const updatedAt = timestamp("updatedAt")
+  .notNull()
+  .$onUpdate(() => new Date());
+
+export const EventTable = pgTable(
+  "events",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    description: text("description"),
+    durationInMinutes: text("duration_in_minutes").notNull(),
+    clerkUserId: text("clerkUserId").notNull(),
+    isActive: boolean("isActive").notNull().default(true),
+    createdAt,
+    updatedAt,
+  },
+  (table) => [index("clerkUserIdIndex").on(table.clerkUserId)]
+);
+
+export const ScheduleTable = pgTable("schedules", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  timezone: text("timezone").notNull(),
+  clerkUserId: text("clerkUserId").notNull(),
+  createdAt,
+  updatedAt,
+});
+
+export const scheduleRelations = relations(ScheduleTable, ({ many }) => ({
+  availabilities: many(ScheduleAvailabilityTable),
+}));
+
+export const scheduleDayOfWeekEnum = pgEnum("day", DAYS_OF_WEEK_IN_ORDER);
+
+export const ScheduleAvailabilityTable = pgTable(
+  "schedule_availabilities",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    scheduleId: uuid("scheduleId")
+      .notNull()
+      .references(() => ScheduleTable.id, { onDelete: "cascade" }),
+    startTime: text("startTime").notNull(),
+    endTime: text("endTime").notNull(),
+    dayOfWeek: scheduleDayOfWeekEnum("dayOfWeek").notNull(),
+  },
+  (table) => [index("scheduleIdIndex").on(table.scheduleId)]
+);
+
+export const ScheduleAbailabilityRelations = relations(
+  ScheduleAvailabilityTable,
+  ({ one }) => ({
+    schedule: one(ScheduleTable, {
+      fields: [ScheduleAvailabilityTable.scheduleId],
+      references: [ScheduleTable.id],
+    }),
+  })
+);
