c5a52792f0f4f4868fedcb7cc3c2eeab1fcb4bfa
Viachaslau Rouski <vrovsky@gmail.com>
2025-05-30 09:59:20 +0100

    done-v0.1-calendoor

--- constants/index.ts
+++ constants/index.ts
@@ -11,5 +11,5 @@ export const DAYS_OF_WEEK_IN_ORDER = [
 export const PrivateNavLinks = [
   { imgURL: "/assets/events.svg", route: "/events", label: "My Events" },
   { imgURL: "/assets/schedule.svg", route: "/schedule", label: "My Schedule" },
-  { imgURL: "/assets/public.svg", route: "/book", label: "Public profile" },
+  // { imgURL: "/assets/public.svg", route: "/book", label: "Public profile" },
 ] as const;
